1a75da6d3b5e9a27bca5a7365750dab63ddc8c26
pablopizarrod <pablopizarro@gmail.com>
2024-01-08 20:49:47 -0300

    Update apiDaemon.py

--- apiDaemon/apiDaemon.py
+++ apiDaemon/apiDaemon.py
@@ -45,8 +45,6 @@ openmrscursor = openmrsdb.cursor()
 try:
     # Consulta para obtener directamente el concept_id basado en el UUID
     openmrscursor.execute(f"SELECT concept_id FROM {openmrsdb_name}.concept WHERE uuid = '{concept_uuid}';")
-    print("*** concept uuid ***")
-    print(concept_uuid)
 
     # Obtener el resultado de la consulta
     concept_id_result = openmrscursor.fetchone()
@@ -84,7 +82,7 @@ while True:
        pa.city_village as comuna_paciente,
        pat_n.value as celular_paciente,
        pat_e.value as email_paciente,
-       o.value_coded as diag_cod,
+       cd.uuid as diag_cod,
        o.creator as usuario_registro
         from """+openmrsdb_name+""".obs o
         inner join """+openmrsdb_name+""".users pr_u on o.creator = pr_u.user_id
@@ -92,6 +90,7 @@ while True:
         inner join """+openmrsdb_name+""".person_name pr_pn on pr_u.person_id = pr_pn.person_id
         inner join """+openmrsdb_name+""".person_name pn on o.person_id = pn.person_id
         inner join """+openmrsdb_name+""".patient_identifier pi on o.person_id = pi.patient_id AND pi.identifier_type = 4
+        inner join """+openmrsdb_name+""".concept cd on o.value_coded = cd.concept_id
         left join """+openmrsdb_name+""".person_address pa on o.person_id = pa.person_id
         left join """+openmrsdb_name+""".person_attribute pat_n on o.person_id = pat_n.person_id and pat_n.person_attribute_type_id = 14
         left join """+openmrsdb_name+""".person_attribute pat_e on o.person_id = pat_e.person_id and pat_e.person_attribute_type_id = 13
@@ -110,7 +109,7 @@ while True:
        pa.city_village as comuna_paciente,
        pat_n.value as celular_paciente,
        pat_e.value as email_paciente,
-       c.condition_coded as diag_cod,
+       cd.uuid as diag_cod,
        -- crt.code as icd10,
        c.creator as usuario_registro
        from """+openmrsdb_name+""".conditions c
@@ -120,6 +119,7 @@ while True:
        inner join """+openmrsdb_name+""".person_name pr_pn on pr_u.person_id = pr_pn.person_id
        inner join """+openmrsdb_name+""".person_name pn on c.patient_id = pn.person_id
        inner join """+openmrsdb_name+""".patient_identifier pi on c.patient_id = pi.patient_id AND pi.identifier_type = 4
+       inner join """+openmrsdb_name+""".concept cd on c.condition_coded = cd.concept_id
        left join """+openmrsdb_name+""".person_address pa on c.patient_id = pa.person_id
        left join """+openmrsdb_name+""".person_attribute pat_n on c.patient_id = pat_n.person_id and pat_n.person_attribute_type_id = 14
        left join """+openmrsdb_name+""".person_attribute pat_e on c.patient_id = pat_e.person_id and pat_e.person_attribute_type_id = 13
@@ -140,46 +140,54 @@ while True:
     openmrsdb.commit()
     openmrscursor.close()
     # Iterar sobre cada resultado obtenido
-    for (obs_id,condition_id,id_notificador,nombre_notificador,nombre_paciente,rut_paciente,direccion_paciente,comuna_paciente,celular_paciente,email_paciente,diad_cod,usuario_registro) in openmrsResult:
+    for (obs_id,condition_id,id_notificador,nombre_notificador,nombre_paciente,rut_paciente,direccion_paciente,comuna_paciente,celular_paciente,email_paciente,diag_cod,usuario_registro) in openmrsResult:
         try:
             
             print("existen resultados, entro al ciclo for para revisarlos por fila ")
-            # Consulto por el detalle del diagnostico
+            # Consulto por el detalle del diagnostico con el uuid
+            
+            diag1 = cielConceptToGesApi.get_concept_details_q(diag_cod)
+            print("*** detalle del diagnostico  por uuid ***")
+            print(diag1)
+            if diag1[0].get('id') is not None:     
+                # Consulto por el detalle del diagnostico con id de ciel
+                diag2 = cielConceptToGesApi.get_concept_details(diag1[0]['id'])          
+                print("*** detalle del diagnostico ***")
+                print(diag2)
+                # Consulto si es posible ges con el codigo de cie10
+                # ges = cielConceptToGesApi.get_who_concept_details(icd10)
 
-            diag = cielConceptToGesApi.get_concept_details(diad_cod)          
-            print("*** detalle del diagnostico ***")
-            print(diag)
-            # Consulto si es posible ges con el codigo de cie10
-            # ges = cielConceptToGesApi.get_who_concept_details(icd10)
+                # Si la respuesta arroja algun ges
+                if diag2.get('ges_concept_id') is not None:
 
-            # Si la respuesta arroja algun ges
-            if diag.get('ges_concept_id') is not None:
-
-                print("es ges")
-                # Consulto los detalles del ges
-                # ges_name = cielConceptToGesApi.get_ges_concept_details(ges[0])
+                    print("es ges")
+                    # Consulto los detalles del ges
+                    # ges_name = cielConceptToGesApi.get_ges_concept_details(ges[0])
                 
-                openmrscursor2 = openmrsdb.cursor()
-                agregar_posible_ges_query = ("INSERT INTO "+notificacionesdb_name+".notificacion_ges (obs_id, condition_id, nombre_establecimiento, direccion_establecimiento, ciudad_establecimiento, notificador_id, nombre_notificador, rut_notificador, nombre_paciente, rut_paciente, aseguradora_paciente, direccion_paciente, comuna_paciente, region_paciente, telefono_fijo_paciente, celular_paciente, email_paciente, cie10, diagnostico_ges, tipo, fechahora_notificacion, firma_notificador, firma_paciente, tipo_notificado, nombre_representante, rut_representante, telefono_fijo_representante, celular_representante, email_representante, fechahora_registro, fechahora_actualizacion, usuario_registro, usuario_actualizacion, estado)"
+                    openmrscursor2 = openmrsdb.cursor()
+                    agregar_posible_ges_query = ("INSERT INTO "+notificacionesdb_name+".notificacion_ges (obs_id, condition_id, nombre_establecimiento, direccion_establecimiento, ciudad_establecimiento, notificador_id, nombre_notificador, rut_notificador, nombre_paciente, rut_paciente, aseguradora_paciente, direccion_paciente, comuna_paciente, region_paciente, telefono_fijo_paciente, celular_paciente, email_paciente, cie10, diagnostico_ges, tipo, fechahora_notificacion, firma_notificador, firma_paciente, tipo_notificado, nombre_representante, rut_representante, telefono_fijo_representante, celular_representante, email_representante, fechahora_registro, fechahora_actualizacion, usuario_registro, usuario_actualizacion, estado)"
                          " VALUES (%s, %s, 'Centro Medico y Dental Fundación', 'Amanda Labarca 70', 'Santiago', %s, %s, null, %s, %s, null, %s, %s, null, null, %s, %s, %s, %s, null, null, null, null, null, null, null, null, null, null, current_timestamp, null, %s, null, 'P')")
                 
-                icd10 = diag['icd10_who_concept_id']
-                ges_name = diag['display_name_ges']
-                # Ejecutar la sentencia SQL
-                openmrscursor2.execute(agregar_posible_ges_query,(obs_id,condition_id,id_notificador,nombre_notificador,nombre_paciente,rut_paciente,direccion_paciente,comuna_paciente,celular_paciente,email_paciente,icd10,ges_name,usuario_registro))
+                    icd10 = diag2['icd10_who_concept_id']
+                    ges_name = diag2['display_name_ges']
+                    # Ejecutar la sentencia SQL
+                    openmrscursor2.execute(agregar_posible_ges_query,(obs_id,condition_id,id_notificador,nombre_notificador,nombre_paciente,rut_paciente,direccion_paciente,comuna_paciente,celular_paciente,email_paciente,icd10,ges_name,usuario_registro))
 
                 
-                # Confirmar los cambios en la base de datos
-                openmrsdb.commit()
-                openmrscursor2.close()
+                    # Confirmar los cambios en la base de datos
+                    openmrsdb.commit()
+                    openmrscursor2.close()
 
+                else:
+                    print("no era ges")
+                if obs_id is not None:
+                    obs_id_inicio = obs_id
+                if condition_id is not None:
+                    condition_id_inicio = condition_id
+                print(obs_id_inicio)
             else:
-                print("no era ges")
-            if obs_id is not None:
-                obs_id_inicio = obs_id
-            if condition_id is not None:
-                condition_id_inicio = condition_id
-            print(obs_id_inicio)
+                print("no encontro diagnostico")
+               
         except Exception as err:
             print(f"Unexpected {err=}, {type(err)=}")
     
